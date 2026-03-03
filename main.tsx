5c2df4c5bdea12a926a4ad38bbbc13a74fa808c5
topanismail999 <topanismail999@gmail.com>
2026-03-03 12:50:24 +0700

    Update main.tsx

--- main.tsx
+++ main.tsx
@@ -3,14 +3,13 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import App from './App';
 import Admin from './Admin';
-import './index.css'; // Pastikan ada file css tailwind Anda
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<App />} />
-        <Route path="/backoffice-admin" element={<Admin />} />
+        <Route path="/panel-admin-rahasia" element={<Admin />} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
